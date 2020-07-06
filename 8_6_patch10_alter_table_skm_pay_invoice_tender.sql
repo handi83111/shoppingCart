f724c4b8b6c17b562518efbf1b72930a03957b51
蔣森儒 <tanner_chiang@17life.com>
2020-07-06 15:31:42 +0800

    tender_detail 添加購物車子編號欄位(sell_order_no_s)

---
+++ 8_6_patch10_alter_table_skm_pay_invoice_tender.sql
@@ -0,0 +1,11 @@
+IF NOT EXISTS
+(
+    SELECT *
+    FROM sys.columns
+    WHERE Name IN(N'sell_order_no_s')
+    AND Object_ID = OBJECT_ID(N'skm_pay_invoice_tender_detail')
+)
+    BEGIN
+        ALTER TABLE [dbo].[skm_pay_invoice_tender_detail]
+        ADD [sell_order_no_s] [int] NULL
+END;
